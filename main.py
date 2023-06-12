6fa8f2e0c8ec5806d2a146740330c95934192e25
Thrit <matheusfnx@hotmail.com>
2023-06-12 23:18:57 +0100

    refactoring the main moving the code to the function.py

--- main.py
+++ main.py
@@ -1,55 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
+
 from modules import function
 
 if __name__ == '__main__':
 
-    product_url = []
-    product_title = []
-    product_price = []
-    product_rating = []
-    product_review = []
-    product_available = []
-
-    headers = (
-        {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0',
-            'Accept-Language': 'en-US'
-        }
-    )
-
     url = r'https://www.amazon.com/s?k=notebook+gamer&i=computers-intl-ship&crid=2MMC2WMSFMH8G&sprefix=note%2Ccomputers-intl-ship%2C191&ref=nb_sb_ss_ts-doa-p_2_4'
+    function.get_data_from_url(url)
 
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.content, 'lxml')
 
-    links = soup.find_all('a', attrs={'class': 'a-link-normal s-no-outline'})
-    links_list = []
 
-    for link in links:
-        links_list.append(link.get('href'))
 
-    for link in links_list:
-
-        url_full = 'https://www.amazon.com{link}'
-        new_page = requests.get(url_full.format(link=link), headers=headers)
-
-        new_page = BeautifulSoup(new_page.content, 'lxml')
-
-        product_url.append(url_full.format(link=link))
-        product_title.append(function_py.get_title(new_page))
-        product_price.append(function_py.get_price(new_page))
-        product_rating.append(function_py.get_rating(new_page))
-        product_review.append(function_py.get_review(new_page))
-        product_available.append(function_py.get_available(new_page))
-
-df_result = function_py.consolidate_results(product_url,
-                                            product_title,
-                                            product_price,
-                                            product_rating,
-                                            product_review,
-                                            product_available
-                                            )
 
 # df_result.to_csv(r'data\result.txt',
 #                  sep=';',
--- modules/function.py
+++ modules/function.py
@@ -1,6 +1,60 @@
 import re
+import requests
 import pandas as pd
+
+from bs4 import BeautifulSoup
 from typing import ClassVar
+
+
+def get_data_from_url(url: str):
+
+    product_url = []
+    product_title = []
+    product_price = []
+    product_rating = []
+    product_review = []
+    product_available = []
+
+    headers = (
+        {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0',
+            'Accept-Language': 'en-US'
+        }
+    )
+
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'lxml')
+
+    links = soup.find_all('a', attrs={'class': 'a-link-normal s-no-outline'})
+    links_list = []
+
+    for link in links:
+        links_list.append(link.get('href'))
+
+    for link in links_list:
+
+        url_full = 'https://www.amazon.com{link}'
+        new_page = requests.get(url_full.format(link=link), headers=headers)
+
+        new_page = BeautifulSoup(new_page.content, 'lxml')
+
+        product_url.append(url_full.format(link=link))
+        product_title.append(get_title(new_page))
+        product_price.append(get_price(new_page))
+        product_rating.append(get_rating(new_page))
+        product_review.append(get_review(new_page))
+        product_available.append(get_available(new_page))
+
+        df_result = consolidate_results(
+            product_url,
+            product_title,
+            product_price,
+            product_rating,
+            product_review,
+            product_available
+        )
+
+    return False
 
 
 def get_title(soup: ClassVar) -> str:
@@ -18,7 +72,6 @@ def get_title(soup: ClassVar) -> str:
         return ''
 
 
-# Function to extract Product Price
 def get_price(soup: ClassVar) -> str:
     """
     Extract the price value.
